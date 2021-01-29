43e357af8bb78afbb40574b69300b65597f1ae4c
Jude Cooray <ranhiru@gmail.com>
2021-01-30 09:38:29 +1100

    Renamed Main class

--- payment-event-consumer/src/main/kotlin/com/ranhiru/twitterstream/PaymentEventConsumerApplication.kt
+++ payment-event-consumer/src/main/kotlin/com/ranhiru/twitterstream/PaymentEventConsumerApplication.kt
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-open class Application {
+open class PaymentEventConsumerApplication {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            SpringApplication.run(Application::class.java, *args)
+            SpringApplication.run(PaymentEventConsumerApplication::class.java, *args)
         }
     }
 }
